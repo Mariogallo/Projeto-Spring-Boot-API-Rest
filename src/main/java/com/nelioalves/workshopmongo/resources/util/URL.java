b21d4a9101a5be0db8d3009af6da5655b1fee7d6
Mariogallo <prof.eng.mariogallo@gmail.com>
2021-08-07 11:51:36 -0300

    Consulta com vários critérios

--- src/main/java/com/nelioalves/workshopmongo/resources/util/URL.java
+++ src/main/java/com/nelioalves/workshopmongo/resources/util/URL.java
@@ -15,5 +15,16 @@ public class URL {
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
-	}	
+	}
+	
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (ParseException e) {
+			return defaultValue;
+		}		
+	}
 }
+
